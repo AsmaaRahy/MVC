ba00227e8f4d96c9ad000dfbca00bec18bd666f1
Asmaa Rahy <111569346+AsmaaRahy@users.noreply.github.com>
2024-03-14 01:20:40 +0200

    Add files via upload

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task1MVC.Models;
+
+namespace Task1MVC.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult BookAppointment()
+        {
+            SampleDataDoctor sample=new SampleDataDoctor();
+            var result=sample.doctors.ToList();
+            return View(result);
+        }
+
+        public ActionResult CompleteAppointment(int id)
+        {
+            SampleDataDoctor sample = new SampleDataDoctor();
+            var result = sample.doctors.Find(e=>e.Id ==id);
+            return View(result);
+        }
+        //Name.Equals(name)
+
+    }
+}
---
+++ Models/Doctor.cs
@@ -0,0 +1,11 @@
+﻿namespace Task1MVC.Models
+{
+        public class Doctor
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Specialization { get; set; }
+            public string Img { get; set; }
+        }
+    }
+
